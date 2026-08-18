618b25dad5ac15e7c72709e7bb05da767159470f
Zain Iftikhar <zain.iftikhar@7vals.com>
2026-08-18 13:00:57 +0500

    Only community resources shown -- Hide queries collection premium alert(count), refs# 53982

--- app/Views/queriesCollection.php
+++ app/Views/queriesCollection.php
@@ -2,14 +2,6 @@
 # Copyright © 2023 FirstWave. All Rights Reserved.
 # SPDX-License-Identifier: AGPL-3.0-or-later
 include 'shared/collection_functions.php';
-if (empty($config->product) or $config->product === 'community') {
-    echo '        <div class="container-fluid">
-            <div class="alert alert-danger alert-dismissable fade show" role="alert">
-                Note that there are actually ' . count($data) . ' reports available for Open-AudIT Professional and Enterprise. Click here to <a href="#"" data-bs-toggle="modal" data-bs-target="#modalCompareLicense">get a free license.</a>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        </div>';
-}
 ?>
         <main class="container-fluid">
             <div class="card">
--- app/Views/summariesCollection.php
+++ app/Views/summariesCollection.php
@@ -20,7 +20,7 @@ include 'shared/collection_functions.php';
                         if ($name === 'collectors' and $collection->count === 0) {
                             continue;
                         }
-                        if (empty($collection->actions->community)) {
+                        if ($collection->edition !== 'Community') {
                             continue;
                         }
                         $i++;
@@ -100,6 +100,9 @@ include 'shared/collection_functions.php';
                         if ($name === 'collectors' and $collection->count === 0) {
                             continue;
                         }
+                        if ($collection->edition !== 'Community') {
+                            continue;
+                        }
                         $i++;
                         $color = 'darkgrey';
                         $link = '#';
@@ -155,58 +158,6 @@ include 'shared/collection_functions.php';
                         }
                     }
                     ?>
-                    </div>
-                </div>
-            </div>
-            <br>
-            <div class="card">
-                <div class="card-header">
-                    <?= collection_card_header($meta->collection, $meta->icon, $user, '', $meta->query_string) ?>
-                </div>
-                <div class="card-body">
-                    <br>
-                    <div class="table-responsive">
-                        <table class="table <?= $GLOBALS['table'] ?> table-striped table-hover dataTable" data-order='[[2,"asc"]]'>
-                            <thead>
-                                <tr>
-                                    <th data-orderable="false" class="text-center"><?= __('Details') ?></th>
-                                    <th data-orderable="false" class="text-center"><?= __('Execute') ?></th>
-                                    <?php foreach ($meta->data_order as $key) {
-                                        if ($key === 'id' or $key === 'orgs.id') {
-                                            continue;
-                                        } ?>
-                                        <th><?= collection_column_name($key) ?></th>
-                                    <?php } ?>
-                                    <?php if (str_contains($user->permissions[$meta->collection], 'd')) { ?>
-                                    <th data-orderable="false" class="text-center"><?= __('Delete') ?></th>
-                                    <?php } ?>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php if (!empty($data)) { ?>
-                                <?php foreach ($data as $item) { ?>
-                                <tr>
-                                    <?= collection_button_read($meta->collection, $item->id) ?>
-                                    <?= collection_button_execute($meta->collection, intval($item->id)) ?>
-                                    <?php foreach ($meta->data_order as $key) {
-                                        if ($key === 'id' or $key === 'orgs.id') {
-                                            continue;
-                                        }
-                                        if ($key === 'orgs.name' and !empty($item->attributes->{'orgs.id'})) {
-                                            echo "<td><a href=\"" . url_to($meta->collection . 'Collection') . "?" . $meta->collection . ".org_id=" . $item->attributes->{'orgs.id'} . "\">" . $item->attributes->{$key} . "</a></td>\n";
-                                        } else {
-                                            echo "<td>" . $item->attributes->{$key} . "</td>\n";
-                                        }
-                                        ?>
-                                    <?php } ?>
-                                    <?php if (str_contains($user->permissions[$meta->collection], 'd')) { ?>
-                                        <?= collection_button_delete(intval($item->id)) ?>
-                                    <?php } ?>
-                                </tr>
-                                <?php } ?>
-                            <?php } ?>
-                            </tbody>
-                        </table>
                     </div>
                 </div>
             </div>
